f0bc39e7b786a35d1c1872d18e200a7a91b7f03f
DominikDeveloper <dominik.garczyk@student.put.poznan.pl>
2017-05-07 01:03:59 +0200

    Verify input lists of vertices/edges

--- program.pl
+++ program.pl
@@ -7,66 +7,45 @@ edge(b,c,bc).
 edge(c,e,ce).
 edge(d,f,df). 
 
-len([],0).
-len([_|T],N)  :-  len(T,X),  N  is  X+1.
 
 list_sum([Item], Item).
 list_sum([Item1,Item2 | Tail], Total) :-
     list_sum([Item1+Item2|Tail], Total).
 	
-if_all_bool([]).
-if_all_bool([H|T]) :-
-    H >= 0, H =< 1, integer(H),
-    if_all_bool(T).
-	
 sum_list([], 0).
 sum_list([H|T], Sum) :-
    sum_list(T, Rest),
-   Sum is H + Rest.
-   
-m2([A|As], [B|Bs], [A,B|Rs]) :-
-    !, m2(As, Bs, Rs).
-	m2([], Bs, Bs) :- !.
-	m2(As, [], As).
+   Sum is H + Rest.	
 	
-merge_list([],L,L ).
-merge_list([H|T],L,[H|M]):-
-    merge_list(T,M,L).
+if_all_bool([]).
+if_all_bool([H|T]) :-
+    H in 0..1,
+    if_all_bool(T).
 	
-
+	
 przyklUni([VertexHead|VertexTail], [EdgeHead|EdgeTail], Sol):-
 	
 	if_all_bool([VertexHead|VertexTail]),
 	if_all_bool([EdgeHead|EdgeTail]),
 	
-	len([VertexHead|VertexTail], VertexNumber),
+	length([VertexHead|VertexTail], VertexNumber),
 	write('L. wierzch.: '), writeln(VertexNumber),
 	sum_list([VertexHead|VertexTail], VertexSum), 
 	write('Suma wartosci wierzch.: '), writeln(VertexSum),
-  
-	A in 0..1,
-	B in 0..1,   
-	C in 0..1,   
-	D in 0..1,  
-	E in 0..1,   
-	F in 0..1,  
-	Ab in 0..1,   
-	Ad in 0..1,   
-	Bd in 0..1,   
-	Bc in 0..1,  
-	Ce in 0..1,   
-	Df in 0..1, 
-	append([VertexHead|VertexTail], [EdgeHead|EdgeTail], Sol),
 	
-	writeln(Sol),
-	A+B+C+D+E+F #=3,
-	Ab #= abs(A-B),
-	Ad #= abs(A-D),
-	Bd #= abs(B-D),
-	Bc #= abs(B-C),
-	Ce #= abs(C-E),
-	Df #= abs(D-F),
-	Ab+Ad+Bd+Bc+Ce+Df #=3,    
+	length([EdgeHead|EdgeTail], EdgeNumber),
+	write('L. krawedzi: '), writeln(EdgeNumber),
+	sum_list([EdgeHead|EdgeTail], EdgeSum), 
+	write('Suma wartosci krawedzi: '), writeln(EdgeSum),
+  
+	append([VertexHead|VertexTail], [EdgeHead|EdgeTail], Sol),
+	VertexSum >= floor(VertexNumber / 2),
+	VertexSum =< ceiling(VertexNumber / 2),	
+	
+	% TODO: Calculate edge values for joined vertices (for example: Ab = abs(A-B))
+	
+	EdgeSum >= floor((EdgeNumber) / 2),
+	EdgeSum =< ceiling((EdgeNumber) / 2),	  
 	labeling([leftmost], Sol).
 
 
